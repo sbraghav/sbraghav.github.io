4d7202eacb8ba059144c0b8f237a71e0c670eaa6
Raghav Balasankaran <sb_raghav@hotmail.com>
2026-05-03 13:43:57 +0530

    Migrate to Astro (#1)

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: 'Raghav Balasankaran',
+    description: 'Developer blog. Writing about code, systems, and things I\'m learning.',
+    site: context.site!,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+}
